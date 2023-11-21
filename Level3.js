48dfdc841e2055546d5136064605589fe3d7497a
pranjal-2507 <pranjalgosavi2507@gmail.com>
2023-11-21 16:12:52 +0530

    Dome with Static part

---
+++ Level3.js
@@ -0,0 +1,36 @@
+const emojis = ['🎈','🎈','🎁','🎁','👑','👑','⚽','⚽','🎖️','🎖️','💡','💡','📒','📒','⏰','⏰','🌼','🌼','🚲','🚲','🚩','🚩','🏠','🏠','🌈','🌈','💙','💙','❓','❓'];
+var shuf_emoji = emojis.sort(()=> (Math.random() > .5) ? 2 : -1);
+
+    for(let i = 0;i<emojis.length;i++){
+        let box = document.createElement("div");
+        box.className = 'item';
+        box.innerHTML = shuf_emoji[i];
+        box.onclick = function(){
+            this.classList.add('boxOpen');
+            setTimeout(function(){
+                if(document.querySelectorAll('.boxOpen').length > 1){
+                    if(document.querySelectorAll(".boxOpen")[0].innerHTML == document.querySelectorAll(".boxOpen")[1].innerHTML){
+                        document.querySelectorAll(".boxOpen")[0].classList.add("boxMatch");
+                        document.querySelectorAll(".boxOpen")[1].classList.add("boxMatch");
+                        
+                        document.querySelectorAll(".boxOpen")[1].classList.remove("boxOpen");
+                        document.querySelectorAll(".boxOpen")[0].classList.remove("boxOpen");
+
+                        if(document.querySelectorAll(".boxOpen").length() === emojis.length()){
+                            alert('Hurray! You won the game')
+                        }
+
+                    }
+                    else{
+                        document.querySelectorAll(".boxOpen")[1].classList.remove("boxOpen");
+                        document.querySelectorAll(".boxOpen")[0].classList.remove("boxOpen");
+                    }
+                }
+            },500)
+        }
+
+
+
+        document.querySelector(".game").appendChild(box);
+
+    }
---
+++ Level_2.js
@@ -0,0 +1,32 @@
+const emojis = ["😀","😀","🥳","🥳","➡️","➡️","🤔","🤔","😍","😍","😓","😓","❤️","❤️","👌","👌","🤖","🤖","🐋","🐋"];
+
+var shuf_emoji = emojis.sort(()=>(Math.random() > .5) ? 2 : -1);
+for(let i=0;i<emojis.length;i++){
+    let box = document.createElement("div");
+    box.className = 'item';
+    box.innerHTML = shuf_emoji[i];
+    box.onclick = function(){
+        this.classList.add('boxOpen');
+        setTimeout(function(){
+            if(document.querySelectorAll('.boxOpen').length > 1){
+                if(document.querySelectorAll(".boxOpen")[0].innerHTML == document.querySelectorAll(".boxOpen")[1].innerHTML){
+                    document.querySelectorAll(".boxOpen")[0].classList.add("boxMatch");
+                        document.querySelectorAll(".boxOpen")[1].classList.add("boxMatch");
+                        
+                        document.querySelectorAll(".boxOpen")[1].classList.remove("boxOpen");
+                        document.querySelectorAll(".boxOpen")[0].classList.remove("boxOpen");
+
+                        if(document.querySelectorAll(".boxOpen").length() === emojis.length()){
+                            alert('Hurray! You won the game')
+                        }
+                }
+                else{
+                    document.querySelectorAll(".boxOpen")[1].classList.remove("boxOpen");
+                    document.querySelectorAll(".boxOpen")[0].classList.remove("boxOpen");
+                }
+            }
+            
+        },500)
+    }
+    document.querySelector('.game').appendChild(box);
+}
