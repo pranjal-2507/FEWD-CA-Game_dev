6013db09b03ed969bfab2f6abac7e4313e37cd93
pranjal-2507 <pranjalgosavi2507@gmail.com>
2023-11-24 09:47:26 +0530

    Done

--- Level3.js
+++ Level3.js
@@ -1,60 +1,89 @@
-let matches =0;
+let matches = 0;
 var currentLevel = 3;
-const emojis = ['🎈','🎈','🎁','🎁','👑','👑','⚽','⚽','🎖️','🎖️','💡','💡','📒','📒','⏰','⏰','🌼','🌼','🚲','🚲','🚩','🚩','🏠','🏠','🌈','🌈','💙','💙','❓','❓'];
-var shuf_emoji = emojis.sort(()=> (Math.random() > .5) ? 2 : -1);
+const emojis = [
+  "🎈",
+  "🎈",
+  "🎁",
+  "🎁",
+  "👑",
+  "👑",
+  "⚽",
+  "⚽",
+  "🎖️",
+  "🎖️",
+  "💡",
+  "💡",
+  "📒",
+  "📒",
+  "⏰",
+  "⏰",
+  "🌼",
+  "🌼",
+  "🚲",
+  "🚲",
+  "🐉",
+  "🐉",
+  "🏠",
+  "🏠",
+  "🌈",
+  "🌈",
+  "💙",
+  "💙",
+  "❓",
+  "❓",
+];
+var shuf_emoji = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
 
-    for(let i = 0;i<emojis.length;i++){
-        let box = document.createElement("div");
-        box.className = 'item';
-        box.innerHTML = shuf_emoji[i];
-        box.onclick = function(){
-            this.classList.add('boxOpen');
-            setTimeout(function(){
-                if(document.querySelectorAll('.boxOpen').length > 1){
-                    if(document.querySelectorAll(".boxOpen")[0].innerHTML == document.querySelectorAll(".boxOpen")[1].innerHTML){
-                        var CardMatch = new Audio("./cardMatchNew.wav");
-                        CardMatch.play();
+for (let i = 0; i < emojis.length; i++) {
+  let box = document.createElement("div");
+  box.className = "item";
+  box.innerHTML = shuf_emoji[i];
+  box.onclick = function () {
+    this.classList.add("boxOpen");
+    setTimeout(function () {
+      if (document.querySelectorAll(".boxOpen").length > 1) {
+        if (
+          document.querySelectorAll(".boxOpen")[0].innerHTML ==
+          document.querySelectorAll(".boxOpen")[1].innerHTML
+        ) {
+          var CardMatch = new Audio("./cardMatchNew.wav");
+          CardMatch.play();
 
-                        document.querySelectorAll(".boxOpen")[0].classList.add("boxMatch");
-                        document.querySelectorAll(".boxOpen")[1].classList.add("boxMatch");
-                        
-                        document.querySelectorAll(".boxOpen")[1].classList.remove("boxOpen");
-                        document.querySelectorAll(".boxOpen")[0].classList.remove("boxOpen");
-                        matches++;
+          document.querySelectorAll(".boxOpen")[0].classList.add("boxMatch");
+          document.querySelectorAll(".boxOpen")[1].classList.add("boxMatch");
 
-                        if(matches == emojis.length/2){
-                            localStorage.setItem('currentLevel', currentLevel)
-                          location.href = 'Win_page.html';
-                        }
+          document.querySelectorAll(".boxOpen")[1].classList.remove("boxOpen");
+          document.querySelectorAll(".boxOpen")[0].classList.remove("boxOpen");
+          matches++;
 
-                    }
-                    else{
-                        document.querySelectorAll(".boxOpen")[1].classList.remove("boxOpen");
-                        document.querySelectorAll(".boxOpen")[0].classList.remove("boxOpen");
-                    }
-                }
-            },500)
+          if (matches == emojis.length / 2) {
+            localStorage.setItem("currentLevel", currentLevel);
+            location.href = "Win_page.html";
+          }
+        } else {
+          document.querySelectorAll(".boxOpen")[1].classList.remove("boxOpen");
+          document.querySelectorAll(".boxOpen")[0].classList.remove("boxOpen");
         }
+      }
+    }, 500);
+  };
 
-            // Set Timer
-    const currentLevel = 3;
-    let seconds = 70
-    let timer = setInterval(()=>{
-        seconds--;
-        document.getElementById("timer").textContent = seconds;
-        if(document.querySelectorAll(".boxOpen").length === emojis.length){
-            localStorage.setItem('currentLevel', currentLevel)
+  // Set Timer
+  const currentLevel = 3;
+  let seconds = 70;
+  let timer = setInterval(() => {
+    seconds--;
+    document.getElementById("timer").textContent = seconds;
+    if (document.querySelectorAll(".boxOpen").length === emojis.length) {
+      localStorage.setItem("currentLevel", currentLevel);
 
-                location.href = 'Win_page.html'
-        }
-        if(seconds == 0 ){
-            localStorage.setItem('currentLevel', currentLevel)
-            location.href = 'Lose_page.html';
-    
-        }
-    },1000)
-
-
-        document.querySelector(".game").appendChild(box);
-
+      location.href = "Win_page.html";
     }
+    if (seconds == 0) {
+      localStorage.setItem("currentLevel", currentLevel);
+      location.href = "Lose_page.html";
+    }
+  }, 1000);
+
+  document.querySelector(".game").appendChild(box);
+}
--- Level_2.js
+++ Level_2.js
@@ -1,57 +1,76 @@
-
 let matches = 0;
 var currentLevel = 2;
-const emojis = ["😀","😀","🥳","🥳","➡️","➡️","🤔","🤔","😍","😍","😓","😓","❤️","❤️","👌","👌","🤖","🤖","🐋","🐋"];
+const emojis = [
+  "😀",
+  "😀",
+  "🥳",
+  "🥳",
+  "➡️",
+  "➡️",
+  "🤔",
+  "🤔",
+  "😍",
+  "😍",
+  "😓",
+  "😓",
+  "❤️",
+  "❤️",
+  "👌",
+  "👌",
+  "🤖",
+  "🤖",
+  "🐋",
+  "🐋",
+];
 
-var shuf_emoji = emojis.sort(()=>(Math.random() > .5) ? 2 : -1);
-for(let i=0;i<emojis.length;i++){
-    let box = document.createElement("div");
-    box.className = 'item';
-    box.innerHTML = shuf_emoji[i];
-    box.onclick = function(){
-        this.classList.add('boxOpen');
-        setTimeout(function(){
-            if(document.querySelectorAll('.boxOpen').length > 1){
-                if(document.querySelectorAll(".boxOpen")[0].innerHTML == document.querySelectorAll(".boxOpen")[1].innerHTML){
-                    var CardMatch = new Audio("./cardMatchNew.wav");
-                    CardMatch.play();
+var shuf_emoji = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+for (let i = 0; i < emojis.length; i++) {
+  let box = document.createElement("div");
+  box.className = "item";
+  box.innerHTML = shuf_emoji[i];
+  box.onclick = function () {
+    this.classList.add("boxOpen");
+    setTimeout(function () {
+      if (document.querySelectorAll(".boxOpen").length > 1) {
+        if (
+          document.querySelectorAll(".boxOpen")[0].innerHTML ==
+          document.querySelectorAll(".boxOpen")[1].innerHTML
+        ) {
+          var CardMatch = new Audio("./cardMatchNew.wav");
+          CardMatch.play();
 
-                    document.querySelectorAll(".boxOpen")[0].classList.add("boxMatch");
-                        document.querySelectorAll(".boxOpen")[1].classList.add("boxMatch");
-                        
-                        document.querySelectorAll(".boxOpen")[1].classList.remove("boxOpen");
-                        document.querySelectorAll(".boxOpen")[0].classList.remove("boxOpen");
-                        matches++;
-                        
-                        if(matches == emojis.length/2){
-                            localStorage.setItem('currentLevel', currentLevel)
-                            window.location.href = 'Win_page.html'
-                        }
-                }
-                else{
-                    document.querySelectorAll(".boxOpen")[1].classList.remove("boxOpen");
-                    document.querySelectorAll(".boxOpen")[0].classList.remove("boxOpen");
-                }
-            }
-            
-        },500)
+          document.querySelectorAll(".boxOpen")[0].classList.add("boxMatch");
+          document.querySelectorAll(".boxOpen")[1].classList.add("boxMatch");
+
+          document.querySelectorAll(".boxOpen")[1].classList.remove("boxOpen");
+          document.querySelectorAll(".boxOpen")[0].classList.remove("boxOpen");
+          matches++;
+
+          if (matches == emojis.length / 2) {
+            localStorage.setItem("currentLevel", currentLevel);
+            window.location.href = "Win_page.html";
+          }
+        } else {
+          document.querySelectorAll(".boxOpen")[1].classList.remove("boxOpen");
+          document.querySelectorAll(".boxOpen")[0].classList.remove("boxOpen");
+        }
+      }
+    }, 500);
+  };
+
+  // Set Timer
+  let seconds = 70;
+  let timer = setInterval(() => {
+    seconds--;
+    document.getElementById("timer").textContent = seconds;
+    if (document.querySelectorAll(".boxOpen").length === emojis.length) {
+      localStorage.setItem("currentLevel", currentLevel);
+      location.href = "Win_page.html";
     }
-
-   
-    // Set Timer
-    let seconds = 70
-    let timer = setInterval(()=>{
-        seconds--;
-        document.getElementById("timer").textContent = seconds;
-        if(document.querySelectorAll(".boxOpen").length === emojis.length){
-            localStorage.setItem('currentLevel', currentLevel)
-                location.href = 'Win_page.html'
-        }
-        if(seconds == 0 ){
-            localStorage.setItem('currentLevel', currentLevel)
-            location.href = 'Lose_page.html';
-    
-        }
-    },1000)
-    document.querySelector('.game').appendChild(box);
+    if (seconds == 0) {
+      localStorage.setItem("currentLevel", currentLevel);
+      location.href = "Lose_page.html";
+    }
+  }, 1000);
+  document.querySelector(".game").appendChild(box);
 }
